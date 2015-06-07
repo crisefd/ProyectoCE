e8cf307ebdc74889cad6027836560252dafd6b7e
crisefd <ophiuco93@hotmail.com>
2015-06-07 17:26:51 -0500

    Cruce uniforme en NReinas

--- aplicacion.rb
+++ aplicacion.rb
@@ -43,20 +43,6 @@ class Cromosoma < Array
 		@diversidad = 0
 		self.sort_by!{rand()}
 	end
-	
-	 #Funcion de cruce uniforme
-	def cruzar!(cromosoma)
-		copia_cromosoma = Array.new(self)
-		iteraciones = @num_genes - 1
-		iteraciones.downto(0){|i|
- 		bit = rand(0..1)
-		if bit == 0 then
-	  		self[i] = copia_cromosoma[i]
-		elsif bit == 1 then
-			self[i] = cromosoma[i]
-		end
-	  	}
-	end
 
 	#Funcion de evaluacion del cromosoma
 	#Es necesario validar que reinas se estan
@@ -172,6 +158,21 @@ class NReinas < Array
   		p "Cromosoma mutado = #{cromosoma_mutado} "
   		cromosoma_mutado
   	end
+  	
+  	#Funcion de cruce uniforme
+	def cruzar(cromosoma1, cromosoma2)
+		nuevo_cromosoma = Cromosoma.new cromosoma1.length
+		iteraciones = @num_genes - 1
+		iteraciones.downto(0){|i|
+  		bit = rand(0..1)
+  		if bit == 0 then
+  			nuevo_cromosoma[i] = cromosoma1[i]
+  		elsif bit == 1 then
+  			nuevo_cromosoma[i] = cromosoma2[i]
+  		end
+  	}
+  	nuevo_cromosoma
+	end
 
 	#Funcion que determina que cromosomas
 	#Pasaran a la siguiente generacion.
